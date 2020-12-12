4078b72c998894cc36b0c84b76e00d6fda6c6a1d
velugotipranay <pranayvelugoti@gmail.com>
2020-12-12 18:32:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function changeColor(x){
+			x.style.backgroundColor="lime";
+			x.style.opacity="1.0";
+			x.style.boxShadow="10px 10px 10px black"
+			x.style.borderRadius="20px";
+		}
+		function retainColor(y){
+			y.style.opacity="0.75";
+			y.style.backgroundColor="aqua";
+			y.style.color="black";
+			y.style.boxShadow="0px 0px 0px transparent"
+			y.style.borderRadius="0px";
+
+		}
+		function changethis(z){
+			z.style.backgroundColor="lime";
+			z.style.color="black";
+			z.style.opacity="0.85";
+		}
+		function retainthis(a){
+			a.style.backgroundColor="black";
+			a.style.color="white";
+			a.style.opacity="1.0";
+		}
