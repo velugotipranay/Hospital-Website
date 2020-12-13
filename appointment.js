2140ec5197680ebb51e66664eadb7b10a5058ff7
velugotipranay <pranayvelugoti@gmail.com>
2020-12-13 12:22:27 +0530

    Update appointment.js

--- appointment.js
+++ appointment.js
@@ -1,22 +1,34 @@
 	function validate(){
-			var Specialist=document.myform.Specialist.value;
-			var exp=/^[A-Za-z\s]+$/ ;
-			var patientname=document.myform.patientname.value;
-			var result=exp.test(patientname);
-			var exp2=/^[0-9]+$/;
-			var phoneNumber=document.myform.phoneNumber.value;
-			var result2=exp2.test(phoneNumber);
-			var exp3=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-			var email=document.myform.email.value;
-			var result3=exp3.test(email);
-			var date=document.getElementById("Date").value;
-			var d=new Date();
-		    var x=d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
-		    var check=date.substr(8,2);
-		    var equal=d.getDate();
-			if(check<equal){
+		var Specialist=document.myform.Specialist.value;
+		var exp=/^[A-Za-z\s]+$/ ;
+		var patientname=document.myform.patientname.value;
+		var result=exp.test(patientname);
+		var exp2=/^[0-9]+$/;
+		var phoneNumber=document.myform.phoneNumber.value;
+		var result2=exp2.test(phoneNumber);
+		var exp3=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+		var email=document.myform.email.value;
+		var result3=exp3.test(email);
+		var date=document.getElementById("Date").value;
+		var d=new Date();
+		var x=d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+		var checkDate=date.substr(8,2);
+		var equalDate=d.getDate();
+		var checkMonth=date.substr(5,2);
+		var equalMonth=d.getMonth();
+		var checkYear=date.substr(0,4);
+		var equalYear=d.getFullYear();
+		    if(checkMonth>=equalMonth){
+		    	if(checkDate<equalDate){
 				alert("Date cannot be less than today!! ");
 				return false;
+				}
+			}
+			else if(checkMonth<equalMonth){
+				if(checkYear<equalYear){
+					alert("Date cannot be less than today!! ");
+					return false;
+				}
 			}
 			if(result){
 				if(result3){
